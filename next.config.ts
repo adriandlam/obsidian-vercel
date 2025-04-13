cf6204a3f52cdb9359d377ff1f0c8d27f39e79ef
Adrian Lam <adrianlamdev@gmail.com>
2025-04-13 12:55:52 -0700

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], // mdx may not be compatible with obsidian
-	typescript: {
-		ignoreBuildErrors: true
-	}
-
+    /* other config options */
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    typescript: {
+        ignoreBuildErrors: true
+    },
+	outputFileTracingIncludes: {
+		'/**/*': ['./content/**/*']
+	},
 };
 
-export default nextConfig
+export default nextConfig;
